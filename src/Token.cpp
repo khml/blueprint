90c31ced620e43fdbdccff767aa09dd860247a35
khml <43922054+khml@users.noreply.github.com>
2020-03-12 22:41:26 +0900

    delete unused.

--- src/Token.cpp
+++ src/Token.cpp
@@ -17,7 +17,7 @@ namespace Lexer
         }
     }
 
-    void lex(const std::string& line, std::vector<Token>& tokens)
+    void tokenize(const std::string& line, std::vector<Token>& tokens)
     {
         std::ostringstream oss;
         tokenKind::Kind kind;
@@ -114,69 +114,5 @@ namespace Lexer
             }
         }
         putString(tokenKind::IDENTIFIER);
-    }
-
-    void multiCharTokenize(std::vector<Token>& tokens)
-    {
-        std::vector<Token> results;
-        auto current = tokens.begin();
-
-        auto addIfNext = [&tokens, &results, &current](tokenKind::Kind&& expect, Token&& token) -> void
-        {
-            if (++current != tokens.end() && current->kind == expect)
-            {
-                results.emplace_back(token);
-                return;
-            }
-            results.emplace_back(*--current);
-        };
-
-        while (current != tokens.end())
-        {
-            switch (current->kind)
-            {
-                case tokenKind::ADD:
-                    addIfNext(tokenKind::ADD, Token(tokenKind::INCREMENTAL, "++"));
-                    break;
-                case tokenKind::SUB:
-                    addIfNext(tokenKind::SUB, Token(tokenKind::DECREMENTAL, "--"));
-                    break;
-                case tokenKind::ASTERISK:
-                    addIfNext(tokenKind::SLASH, Token(tokenKind::COMMENT_END, "*/"));
-                    break;
-                case tokenKind::SLASH:
-                    addIfNext(tokenKind::ASTERISK, Token(tokenKind::COMMENT_START, "/*"));
-                    break;
-                case tokenKind::EQUAL:
-                    addIfNext(tokenKind::EQUAL, Token(tokenKind::EQUIVALENCE, "=="));
-                    break;
-                case tokenKind::GRATER_THAN:
-                    addIfNext(tokenKind::EQUAL, Token(tokenKind::GRATER, ">="));
-                    break;
-                case tokenKind::LESSER_THAN:
-                    addIfNext(tokenKind::EQUAL, Token(tokenKind::LESSER, "<="));
-                    break;
-                case tokenKind::AMPERSAND:
-                    addIfNext(tokenKind::AMPERSAND, Token(tokenKind::AND, "&&"));
-                    break;
-                case tokenKind::PIPE:
-                    addIfNext(tokenKind::PIPE, Token(tokenKind::OR, "||"));
-                    break;
-                case tokenKind::WHITESPACE:
-                    // get rid of whitespace
-                    break;
-                default:
-                    results.emplace_back(*current);
-                    break;
-            }
-            current++;
-        }
-        tokens.swap(results);
-    }
-
-    void tokenize(const std::string& line, std::vector<Token>& tokens)
-    {
-        lex(line, tokens);
-        // multiCharTokenize(tokens);
     }
 }
