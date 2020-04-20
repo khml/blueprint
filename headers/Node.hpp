d8f85bee57ab9484157288cc464fe50cfb3870ad
khml <43922054+khml@users.noreply.github.com>
2020-04-20 20:35:07 +0900

    AST Node

--- headers/Node.hpp
+++ headers/Node.hpp
@@ -11,6 +11,8 @@
 #include <sstream>
 #endif
 
+#include "Tokenizer.hpp"
+
 namespace AST
 {
     class Node
@@ -38,6 +40,72 @@ namespace AST
 
     private:
         void graph(std::ostringstream& dotFile);
+
+#endif
+    };
+
+    class AstNode
+    {
+    public:
+        explicit AstNode(const Lexer::Token& token);
+
+        virtual ~AstNode();
+
+        const Lexer::Token token;
+
+#ifdef DEBUG_NODE
+
+        virtual void print();
+
+#endif
+
+#ifdef DEBUG_GRAPH
+        int objId = 0;
+
+        virtual void graph();
+
+        virtual void graph(std::ostringstream& dotFile);
+
+#endif
+    };
+
+    class PrimaryNode : public AstNode
+    {
+    public:
+        explicit PrimaryNode(const Lexer::Token& token);
+
+        ~PrimaryNode() override;
+    };
+
+    class VariableNode : public AstNode
+    {
+    public:
+        explicit VariableNode(const Lexer::Token& token);
+
+        ~VariableNode() override;
+    };
+
+    class BinaryOpNode : public AstNode
+    {
+    public:
+        explicit BinaryOpNode(const Lexer::Token& token);
+
+        ~BinaryOpNode() override;
+
+#ifdef DEBUG_NODE
+
+        void print() override;
+
+#endif
+
+        std::unique_ptr<AstNode> left;
+        std::unique_ptr<AstNode> right;
+
+    protected:
+
+#ifdef DEBUG_GRAPH
+
+        void graph(std::ostringstream& dotFile) override;
 
 #endif
     };
--- src/Node.cpp
+++ src/Node.cpp
@@ -69,4 +69,89 @@ namespace AST
             right->graph(dotFile);
     }
 #endif
+
+    AstNode::AstNode(const Lexer::Token& token) :token(token)
+    {}
+
+    AstNode::~AstNode()
+    = default;
+
+#ifdef DEBUG_NODE
+    void AstNode::print()
+    {
+        std::cerr << "node: " << token.value << std::endl;
+    }
+#endif
+
+#ifdef DEBUG_GRAPH
+    void AstNode::graph()
+    {
+        std::ostringstream dotFile;
+        dotFile
+            << "digraph AST {" << std::endl
+            << "  graph [" << std::endl
+            << "    dpi = 300;" << std::endl
+            << "    ratio = 0.5;" << std::endl
+            << "  ]" << std::endl;
+        graph(dotFile);
+        dotFile << "}" << std::endl;
+
+        std::cerr << dotFile.str() << std::endl;
+    }
+
+    void AstNode::graph(std::ostringstream& dotFile)
+    {
+        dotFile << "  " << objId << " [ label = \"" << token.value << "\" ]" << std::endl;
+    }
+#endif
+
+    PrimaryNode::PrimaryNode(const Lexer::Token& token) :AstNode(token)
+    {}
+
+    PrimaryNode::~PrimaryNode()
+    = default;
+
+    VariableNode::VariableNode(const Lexer::Token& token) :AstNode(token)
+    {}
+
+    VariableNode::~VariableNode()
+    = default;
+
+    BinaryOpNode::BinaryOpNode(const Lexer::Token& token) :AstNode(token), left(nullptr), right(nullptr)
+    {}
+
+    BinaryOpNode::~BinaryOpNode()
+    = default;
+
+#ifdef DEBUG_NODE
+    void BinaryOpNode::print()
+    {
+        std::cerr << "node: " << token.value;
+        if (left)
+            std::cerr << ", left: " << left->token.value;
+        if (right)
+            std::cerr << ", right: " << right->token.value;
+        std::cerr << std::endl;
+
+        if (left)
+            left->print();
+        if (right)
+            right->print();
+    }
+#endif
+
+#ifdef DEBUG_GRAPH
+    void BinaryOpNode::graph(std::ostringstream& dotFile)
+    {
+        AstNode::graph(dotFile);
+        if (left != nullptr)
+            dotFile << "  " << objId << "->" << left->objId << std::endl;
+        if (right != nullptr)
+            dotFile << "  " << objId << "->" << right->objId << std::endl;
+        if (left != nullptr)
+            left->graph(dotFile);
+        if (right != nullptr)
+            right->graph(dotFile);
+    }
+#endif
 }
