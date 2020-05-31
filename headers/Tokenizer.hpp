f05013871397a6c170ccb5283de5c259ce1c74a2
khml <43922054+khml@users.noreply.github.com>
2020-05-31 17:59:11 +0900

    filename and row attributes

--- headers/Tokenizer.hpp
+++ headers/Tokenizer.hpp
@@ -23,6 +23,12 @@ namespace Lexer
 
         Token(tokenKind::Kind kind, tokenType::Type type);
 
+        Token(tokenKind::Kind kind, std::string value, std::string filename, uint16_t row);
+
+        Token(tokenKind::Kind kind, std::string value, tokenType::Type type, std::string filename, uint16_t row);
+
+        Token(tokenKind::Kind kind, tokenType::Type type, std::string filename, uint16_t row);
+
         Token(const Token& orig);
 
         virtual ~Token();
@@ -30,6 +36,8 @@ namespace Lexer
         const tokenKind::Kind kind;
         const tokenType::Type type;
         const std::string value;
+        const std::string filename;
+        const uint16_t row;
 
         void print();
     };
