b29be23824120216bf2322a162b8b75974838c81
khml <43922054+khml@users.noreply.github.com>
2020-06-07 17:20:54 +0900

    Tokenizer and Token detach

--- headers/Tokenizer.hpp
+++ headers/Tokenizer.hpp
@@ -8,39 +8,10 @@
 #include <string>
 #include <vector>
 
-#include "TokenKind.hpp"
-#include "TokenType.hpp"
+#include "Token.hpp"
 
 namespace Lexer
 {
-    class Token
-    {
-    public:
-        Token(tokenKind::Kind kind, const std::string& value);
-
-        Token(tokenKind::Kind kind, std::string value, tokenType::Type type);
-
-        Token(tokenKind::Kind kind, tokenType::Type type);
-
-        Token(tokenKind::Kind kind, std::string value, std::string filename, uint16_t row);
-
-        Token(tokenKind::Kind kind, std::string value, tokenType::Type type, std::string filename, uint16_t row);
-
-        Token(tokenKind::Kind kind, tokenType::Type type, std::string filename, uint16_t row);
-
-        Token(const Token& orig);
-
-        virtual ~Token();
-
-        const tokenKind::Kind kind;
-        const tokenType::Type type;
-        const std::string value;
-        const std::string filename;
-        const uint16_t row;
-
-        void print();
-    };
-
     class Tokenizer
     {
     public:
